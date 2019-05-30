0b5a5fe15ec205fbb9f4ec194b6300d37d2dc52d
Rolandinovich <mromanmail@gmail.com>
2019-05-30 15:15:22 +0300

    5е ДЗ готово

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,33 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+from collections import namedtuple
+
+QUARTERS = 4
+Company = namedtuple('Company', ['name', 'quarters', 'profit'])
+all_companies = set()
+num = int(input('Введите количество предприятий: \n'))
+total_profit = 0
+for i in range(1, num + 1):
+    profit = 0
+    quarters = []
+    name = input(f'Введите название предприятия: \n')
+    for j in range(QUARTERS):
+        quarters.append((int(input(f'Прибыль за {j + 1}-й квартал: '))))
+        profit += quarters[j]
+    comp = Company(name=name, quarters=tuple(quarters), profit=profit)
+
+    all_companies.add(comp)
+    total_profit += profit
+average = total_profit / num
+print(f'\n Средняя прибыль = {average}')
+print(f'\n Предприятия с прибылью выше среднего:')
+for comp in all_companies:
+    if comp.profit > average:
+        print(f'Предприятие {comp.name} заработало {comp.profit}')
+
+print(f'\n Предприятия с прибылью ниже среднего:')
+for comp in all_companies:
+    if comp.profit < average:
+        print(f'Предприятие {comp.name} заработало {comp.profit}')
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,76 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+from collections import deque
+
+BASE = 16
+
+HEX_NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+BIN_NUMBERS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'A': 10, 'B': 11,
+               'C': 12, 'D': 13, 'E': 14, 'F': 15}
+
+
+def convert(hex_num):
+    deq_hex_num = deque(hex_num.upper())
+    return deq_hex_num
+
+
+def sum_hex(first, second):
+    first = first.copy()
+    second = second.copy()
+
+    if len(second) > len(first):
+        first, second = second, first
+
+    second.extendleft('0' * (len(first) - len(second)))
+
+    result = deque()
+    overflow = 0
+    for i in range(len(first) - 1, -1, -1):
+        first_num = BIN_NUMBERS[first[i]]
+        second_num = BIN_NUMBERS[second[i]]
+        result_num = first_num + second_num + overflow
+
+        if result_num >= BASE:
+            overflow = 1
+            result_num -= BASE
+        else:
+            overflow = 0
+        result.appendleft((HEX_NUMBERS[result_num]))
+
+    if overflow == 1:
+        result.appendleft('1')
+
+    return result
+
+
+def mult_hex(first, second):
+    first = first.copy()
+    second = second.copy()
+
+    if len(second) > len(first):
+        first, second = second, first
+
+    second.extendleft('0' * (len(first) - len(second)))
+
+    result = deque('0')
+
+    for i in range(len(first) - 1, -1, -1):
+        second_num = BIN_NUMBERS[second[i]]
+
+        spam = deque('0')
+        for _ in range(second_num):
+            spam = sum_hex(spam, first)
+
+        spam.extend('0' * (len(first) - i - 1))
+
+        result = sum_hex(result, spam)
+    return result
+
+
+a = input('Введите первое число в hex формате (только цифры от 0 до f): ')
+b = input('Введите первое число в hex формате (только цифры от 0 до f): ')
+a = convert(a)
+b = convert(b)
+print(f'{list(a)} + {list(b)} = {list(sum_hex(a, b))}')
+print(f'{a} * {b} = {mult_hex(a, b)}')
