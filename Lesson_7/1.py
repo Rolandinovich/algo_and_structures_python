41196a3a6fe2e64e134ac98fb9611f24f0d1808d
Роман <mromanmail@gmail.com>
2019-06-05 19:03:52 +0300

    ДЗ 7 начало

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,26 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+
+import random
+
+array = [random.randint(-100, 100) for _ in range(20)]
+print(array)
+
+
+def bubble_sort(array):
+    n = 1
+    len_array = len(array)
+    while n < len(array):
+        for i in range(len_array - 1):
+            if array[i] < array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+                if len_array - 2 == i:
+                    # в качестве улучшения перестаю сравнивать последние эллементы массива
+                    # так как они уже на своём месте
+                    len_array -= 1
+        n += 1
+
+
+bubble_sort(array)
+print(array)
--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,44 @@
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
 """
+#http://py-algorithm.blogspot.com/2012/01/blog-post.html
+import random
+
+array = [random.random() * 50 for _ in range(20)]
+[print(a) for a in array]
+
+
+def MergerSort(a):
+    def MergerGroup(a, left, m, right):
+        if left >= right:
+            return None
+        if m < left or right < m:
+            return None
+        t = left
+        for j in range(m + 1, right + 1):  # подгруппа 2
+            for i in range(t, j):  # цикл подгруппы 1
+                if a[j] < a[i]:
+                    r = a[j]
+                    # итерационно переставляем элементы, чтобы упорядочить
+                    for k in range(j, i, -1):
+                        a[k] = a[k - 1]
+                    a[i] = r
+                    t = i  # проджолжение вставки в группе 1
+                    break  # к следующему узлу из подгруппы 2
+
+    if len(a) < 2:
+        return None
+    k = 1
+    while k < len(a):
+        g = 0
+        while g < len(a):  # группы
+            z = g + k + k - 1  # последний эл-т группы
+            r = z if z < len(a) else len(a) - 1  # последняя группа
+            MergerGroup(a, g, g + k - 1, r)  # слияние
+            g += 2 * k
+        k *= 2
+
+
+MergerSort(array)
+print('Отсортированный массив')
+[print(a) for a in array]
